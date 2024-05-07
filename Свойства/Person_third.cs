60ff5655df3a1eba3afb523a73f372c70498a921
Дмитрий Панов <Lemrik33work@gmail.com>
2024-05-07 16:09:33 +0300

    properties part 2
    свойства часть 2

---
+++ Свойства/Person_third.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Свойства
+{
+    internal class Person_third
+    {
+        string firstName;
+        string lastName;
+        public string Name
+        {
+            get 
+            {
+                return $"{firstName} {lastName}";
+            }
+        }
+
+        // конструктор
+        public Person_third(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+}
