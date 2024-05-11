a44a7249aa9f0079cea54e15ba02c3a702984f83
Дмитрий Панов <Lemrik33work@gmail.com>
2024-05-11 23:40:47 +0300

    const, fields and readonly
    Константы, поля и структуры для чтения

---
+++ Константы, поля и структуры для чтения/Person_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Константы__поля_и_структуры_для_чтения
+{
+    internal class Person_1
+    {
+
+        /*
+            Поля для чтения представляют такие поля класса или структуры, 
+            значение которых нельзя изменить. 
+            Таким полям можно присвоить значение либо при непосредственно при их объявлении, либо в конструкторе. 
+            В других местах программы присваивать значение таким полям нельзя, можно только считывать их значение.
+
+            Поле для чтения объявляется с ключевым словом readonly:
+         */
+
+        public readonly string name = "Undefined"; // можно так инициализировать
+        public Person_1(string name)
+        {
+            this.name = name; // в конструкторе также можно присвоить значение полю для чтения
+        }
+        public void ChangeName(string otherName)
+        {
+            //this.name = otherName;    // так нельзя 
+        }
+    }
+}
